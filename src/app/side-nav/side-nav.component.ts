d52c58f19ce66280d63673e4cf1c9fed92fe2c69
Ayush Maheshwari <amaheshwari@simeiosolutions.com>
2020-09-15 11:47:57 +0530

    Closing sidenav on click of navigation item on handsets

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -6,16 +6,33 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.css'],
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit {
+  isHandset$: Observable<boolean>;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) {}
+
+  ngOnInit(): void {
+    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+      map((result) => result.matches),
       shareReplay()
     );
+  }
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  closeSidenav(drawer: any): void {
+    this.breakpointObserver
+      .observe(Breakpoints.Handset)
+      .pipe(
+        map((result) => result.matches),
+        shareReplay()
+      )
+      .subscribe((res) => {
+        if (res) {
+          drawer.toggle();
+        }
+      });
+  }
 }
